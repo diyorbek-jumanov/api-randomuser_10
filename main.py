0fd80c16d9d97a99311344b2e2f1f6c289074e05
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-01-28 03:10:44 +0000

    creat full data

--- main.py
+++ main.py
@@ -9,4 +9,26 @@ while sum_female <= 10 or sum_male <= 10:
     url = 'https://randomuser.me/api'
     r = requests.get(url)
     data = r.json()
-    
+    if r.status_code == 200:
+        name = data['results'][0]['name']
+        full_name = name['last'] + ' ' + name['first']
+        gender = data['results'][0]['gender']
+
+        if gender == 'male' and sum_male <= 10:
+            male_data = {
+                'fullName': full_name
+            }
+            male_list.append(male_data)
+            sum_male += 1
+
+        if gender == 'female' and sum_female <= 10:
+            female_data = {
+                'fullName': full_name
+            }
+            female_list.append(female_data)
+            sum_female += 1
+
+full_list = [
+    male_list,
+    female_list
+]
